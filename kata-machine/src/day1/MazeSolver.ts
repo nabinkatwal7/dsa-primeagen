a42d8a58a66aca5223c9b265896b445560faa977
Nabin Katwal <91274113+nabinkatwal7@users.noreply.github.com>
2024-01-22 20:45:15 +0545

    recursive base case

--- kata-machine/src/day1/MazeSolver.ts
+++ kata-machine/src/day1/MazeSolver.ts
@@ -1,3 +1,26 @@
+function walk(maze:string[], wall:string, curr: Point, end: Point, seen:boolean[][]):boolean{
+    // Base case 
+    // 2. off the map
+    if (curr.x < 0 || curr.x >= maze[0].length || curr.y <0 || curr.y >= maze.length) {
+        return false;
+    }
+
+    // 2. Are we on a wall? 
+    if(maze[curr.y][curr.x] === wall) {
+        return false;
+    }
+
+    if (curr.x === end.x && curr.y === end.y) {
+        return true;
+    }
+
+    if (seen[curr.y][curr.x]) {
+        return false
+    }
+
+
+}
+
 export default function solve(maze: string[], wall: string, start: Point, end: Point): Point[] {
 
 }
